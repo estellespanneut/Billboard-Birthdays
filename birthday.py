b24fbf89f5460a1886200db8f6c25065677358b1
Estelle Spanneut <es1342@georgetown.edu>
2021-04-28 17:50:48 -0400

    Basic requirements

---
+++ birthday.py
@@ -0,0 +1,7 @@
+import billboard
+chart = billboard.ChartData('country-songs')
+print(chart.title)
+song = chart[0]  # Get no. 1 song on chart
+print(song.title)
+print(song.artist)
+print(song.weeks)  # Number of weeks on chart
