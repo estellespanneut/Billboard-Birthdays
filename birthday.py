73feef41e297febd75df5760763be1560b25885c
Shana Shin <sshshana@gmail.com>
2021-05-01 00:29:37 +0900

    birth date & chart type input validations
    I set up birthday.py file according to the structure of weather_service.py file in the app folder of Daily Briefings project.
    
    It seems like billboard.py package handles value errors gracefully, so I didn't add additional input validation.

--- birthday.py
+++ birthday.py
@@ -1,7 +1,51 @@
 import billboard
-chart = billboard.ChartData('country-songs')
-print(chart.title)
-song = chart[0]  # Get no. 1 song on chart
-print(song.title)
-print(song.artist)
-print(song.weeks)  # Number of weeks on chart
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from datetime import datetime
+from app import APP_ENV
+
+# .env file should have SENDGRID_API_KEY and SENDER_EMAIL_ADDRESS variables
+load_dotenv()
+
+#
+# user input
+#
+
+BIRTH_DATE = os.getenv("BIRTH_DATE", default="2000-01-01")
+CHART_TYPE = os.getenv("CHART_TYPE", default="hot-100")
+
+def set_birth_date():
+    if APP_ENV == "development":
+        birth_date = input("""
+            Please enter your birth date in YYYY-MM-DD format.
+            For example, if your birth date is April 5th, 2000, enter '2000-04-05.'
+            """)
+    else:
+        birth_date = BIRTH_DATE
+    return birth_date
+
+def set_chart_type():
+    if APP_ENV == "development":
+        chart_type = input("""
+            Please enter the desired Billboard chart type to retrieve data from.
+            A few charts available are 'hot-100', 'country-songs', 'pop-songs', 'r-b-hip-hop-songs.'
+            If you want to view an entire list of available charts, please enter 'list'.
+            """)
+    else:
+        chart_type = CHART_TYPE
+    return chart_type
+
+birth_date = set_birth_date()
+chart_type = set_chart_type()
+
+# show all the available billbaord charts to the user
+if chart_type=="list":
+    print(sorted(billboard.charts()))
+    chart_type = input("Please select the desired billboard chart type from the list.")
+
+# other value errors are handled by billboard.py --> no need to add more input validation
+
+chart = billboard.ChartData(chart_type, birth_date)
+print(chart)
