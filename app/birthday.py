db3f3e8e50ab8450b3791843d98e8e91fbc6c03f
sshshana <77857640+sshshana@users.noreply.github.com>
2021-05-15 04:54:37 +0900

    all of Shana's edits (website &design elements) (#4)
    * add dropdown menu for selecting a chart type
    
    I have an error with "import billboard" line in app/billboard.py file :(
    
    * Update birthday.py
    
    * Design edits
    
    * add local sendgrid app
    
    * Update email_service.py
    
    * delete unnecessary/repetitive files
    
    * new email & website design
    
    * integrate sendgrid feature to the birthday app
    
    * email reflects retrieved billboard data based on user inputs
    
    YAS
    
    * email integration in web app
    
    * Update birthday_billboard.html
    
    Co-authored-by: Shana Shin <sshshana@gmail.com>
    Co-authored-by: Estelle Spanneut <es1342@georgetown.edu>

--- app/birthday.py
+++ app/birthday.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from app import APP_ENV
 import billboard #added
 import json #added
+import ast
 
 # .env file should have SENDGRID_API_KEY and SENDER_EMAIL_ADDRESS variables
 load_dotenv()
@@ -16,6 +17,9 @@ load_dotenv()
 
 BIRTH_DATE = os.getenv("BIRTH_DATE", default="2000-01-01")
 CHART_TYPE = os.getenv("CHART_TYPE", default="hot-100")
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+SENDER_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL_ADDRESS")
+TEMPLATE_ID = "d-05507d891d5b41248bf693b643ec804d" #sendgrid email template
 
 def set_birth_date():
     if APP_ENV == "development":
@@ -31,16 +35,30 @@ def set_chart_type():
     if APP_ENV == "development":
         chart_type = input("""
             Please enter the desired Billboard chart type to retrieve data from.
-            A few charts available are 'hot-100', 'country-songs', 'pop-songs', 'r-b-hip-hop-songs.'
+            Some charts available include 'hot-100', 'country-songs', 'pop-songs', 'r-b-hip-hop-songs.'
             If you want to view an entire list of available charts, please enter 'list'.
             """)
     else:
         chart_type = CHART_TYPE
     return chart_type
 
+def set_email():
+    if APP_ENV == "development":
+        email = input("Please enter 'y' if you want to send an email. If not, enter 'n.'")
+    else:
+        email = 'n'
+    return email 
 
+def set_recipient_email_address():
+    if APP_ENV == "development":
+        recipient_address = input("Please enter a recipient's email address")
+    else:
+        recipient_address = 'ss4012@georgetown.edu'
+    return recipient_address
 
-# other value errors are handled by billboard.py --> no need to add more input validation
+#
+# retrieve data from billboard chart
+# 
 
 def get_chart(chart_type, birth_date):
     chart = billboard.ChartData(chart_type, birth_date)
@@ -53,17 +71,68 @@ def get_chart(chart_type, birth_date):
         clean_data.append(data["title"])
     return clean_data
 
+def get_chart_for_email(chart_type, birth_date):
+    chart = billboard.ChartData(chart_type, birth_date)
+    chart_for_email = []
+    for entry in chart.entries:
+        data = {
+        "title": entry.title,
+        "artist":entry.artist
+        }
+        chart_for_email.append(data)
+    json.dumps(chart_for_email)
+    return chart_for_email
+
+#
+# send an email using sendgrid application
+#
+
+def SendDynamic():
+    """ Send a dynamic email to a list of email addresses
+
+    :returns API response code
+    :raises Exception e: raises an exception """
+    # create Mail object and populate
+    message = Mail(
+        from_email=SENDER_EMAIL_ADDRESS,
+        to_emails=RECIPIENT_EMAIL_ADDRESS)
+    # pass custom values for our HTML placeholders
+    message.dynamic_template_data = {
+        'subject': 'Billboard Chart on Your Birthday!',
+        'birth_date': birth_date,
+        'chart_type': chart_type,
+        'chart_for_email': chart_for_email
+    }
+    message.template_id = TEMPLATE_ID
+    # create our sendgrid client object, pass it our key, then send and return our response objects
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        code, body, headers = response.status_code, response.body, response.headers
+        print("Response code:", code)
+        print("Response headers:", headers)
+        print("Response body:", body)
+        print("Dynamic Messages Sent!")
+    except Exception as e:
+        print("Error: {0}".format(e))
+    return str(response.status_code)
+
+#
+# run the app
+#
 
 if __name__ == "__main__":
 
     birth_date = set_birth_date()
     chart_type = set_chart_type()
 
-    # show all the available billboard charts to the user
-    if chart_type=="list":
-        print(sorted(billboard.charts()))
-        chart_type = input("Please select the desired billboard chart type from the list.")
+    clean_data = get_chart(chart_type, birth_date)
+    print(clean_data)
 
-    print_chart = get_chart(chart_type, birth_date) #test
-    print(print_chart) #test
+    send_email = set_email()
 
+    if send_email == 'y':
+        chart_for_email = get_chart_for_email(chart_type, birth_date)
+        RECIPIENT_EMAIL_ADDRESS = set_recipient_email_address()
+        SendDynamic()
+        print(chart_for_email)
