3e5197cc8af035ba49b64e9ff5e1a02e54fe024f
Shana Shin <sshshana@gmail.com>
2021-05-14 17:13:18 +0900

    new email & website design

--- app/email_service.py
+++ app/email_service.py
@@ -10,47 +10,35 @@ load_dotenv()
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 SENDER_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL_ADDRESS")
 
+TEMPLATE_ID = "d-05507d891d5b41248bf693b643ec804d"
 
-def send_email(subject="[Birtday and Songs] This is a test", html="<p>Hello World</p>", recipient_address=SENDER_EMAIL_ADDRESS):
-    """
-    Sends an email with the specified subject and html contents to the specified recipient,
-    If recipient is not specified, sends to the admin's sender address by default.
-    """
-    client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
-    print("CLIENT:", type(client))
-    print("SUBJECT:", subject)
-    #print("HTML:", html)
+def SendDynamic():
+    """ Send a dynamic email to a list of email addresses
 
-    message = Mail(from_email=SENDER_EMAIL_ADDRESS, to_emails=recipient_address, subject=subject, html_content=html)
+    :returns API response code
+    :raises Exception e: raises an exception """
+    # create Mail object and populate
+    message = Mail(
+        from_email=SENDER_EMAIL_ADDRESS,
+        to_emails=SENDER_EMAIL_ADDRESS)
+    # pass custom values for our HTML placeholders
+    message.dynamic_template_data = {
+        'subject': 'Billboard Chart on Your Birthday!',
+    }
+    message.template_id = TEMPLATE_ID
+    # create our sendgrid client object, pass it our key, then send and return our response objects
     try:
-        response = client.send(message)
-        print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
-        print(response.status_code) #> 202 indicates SUCCESS
-        return response
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        code, body, headers = response.status_code, response.body, response.headers
+        print("Response code:", code)
+        print("Response headers:", headers)
+        print("Response body:", body)
+        print("Dynamic Messages Sent!")
     except Exception as e:
-        print("OOPS", type(e), e.message)
-        return None
+        print("Error: {0}".format(e))
+    return str(response.status_code)
 
 
 if __name__ == "__main__":
-    example_subject = "[Birthday and Songs] This is a test"
-
-    example_html = """
-    <img src="https://images-na.ssl-images-amazon.com/images/I/71uqL5rr8%2BL._AC_SL1500_.jpg" alt="Trulli" width="700" height="275">
-
-    <h3 style="color:purple">This is a test of the Billboard Birthday service</h3>
-    
-    <h4>Today's Date</h4>
-    <p>Monday, January 1, 2040</p>
-    <h4>Billboard Songs</h4>
-    <ul>
-        <li>1. song 1 | artist 1%</li>
-        <li>2. song 2 | artist 2</li>
-        <li>3. song 3 | artist 3</li>
-    </ul>
-
-    <h5 style="background-color:Violet;" height="700"> </h5>
-
-    """
-
-    send_email(example_subject, example_html)
+    SendDynamic()
