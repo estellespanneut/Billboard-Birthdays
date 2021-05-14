c3d1fdd181556090222fa1226e73158d3e3f1a26
Estelle Spanneut <es1342@georgetown.edu>
2021-05-14 15:32:26 -0400

    email integration in web app

--- app/email_service.py
+++ app/email_service.py
@@ -11,11 +11,12 @@ SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 SENDER_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL_ADDRESS")
 
 
-def send_email(subject="[Birtday and Songs] This is a test", html="<p>Hello World</p>", recipient_address=SENDER_EMAIL_ADDRESS):
+def send_email(subject="[Birthday and Songs] This is a test", html="<p>Hello World</p>", recipient_address=SENDER_EMAIL_ADDRESS):
     """
     Sends an email with the specified subject and html contents to the specified recipient,
     If recipient is not specified, sends to the admin's sender address by default.
     """
+
     client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
     print("CLIENT:", type(client))
     print("SUBJECT:", subject)
