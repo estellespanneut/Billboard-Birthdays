78d24b427a9a900a8abc969c7900c865ac4d965b
Estelle Spanneut <es1342@georgetown.edu>
2021-05-15 17:54:15 -0400

    form updates

--- web_app/routes/birthday_routes.py
+++ web_app/routes/birthday_routes.py
@@ -58,27 +58,3 @@ def birthday_billboard(): #check this
         flash("Error. Please try again!", "danger")
         return redirect("/birthday/form")
 
-@birthday_routes.route("/birthday/email", methods=["GET", "POST"])
-def birthday_email(): #check this
-    print("Email sending!")
-
-    if request.method == "GET":
-        print("URL PARAMS:", dict(request.args))
-        request_data = dict(request.args)
-    elif request.method == "POST": # the form will send a POST
-        print("FORM DATA:", dict(request.form))
-        request_data = dict(request.form)
-
-    BIRTH_DATE = request_data.get("birth_date") or "2000-01-01" 
-    CHART_TYPE = request_data.get("chart_type") or "hot-100"
-    CHART_FOR_EMAIL = get_chart_for_email(birth_date = BIRTH_DATE, chart_type = CHART_TYPE)
-    SENDER_EMAIL_ADDRESS = request_data.get("SENDER_EMAIL_ADDRESS") or "example@example.com"
-    results = SendDynamic(SENDER_EMAIL_ADDRESS = SENDER_EMAIL_ADDRESS, RECIPIENT_EMAIL_ADDRESS = SENDER_EMAIL_ADDRESS, birth_date = BIRTH_DATE, chart_type = CHART_TYPE, chart_for_email = CHART_FOR_EMAIL)
-    #results = SendDynamic(SENDER_EMAIL_ADDRESS = SENDER_EMAIL_ADDRESS, RECIPIENT_EMAIL_ADDRESS = SENDER_EMAIL_ADDRESS) #recipient_address = SENDER_EMAIL_ADDRESS
-    
-    if results:
-        flash("Email sent successfully!", "success")
-        return render_template("email.html", recipient_address = SENDER_EMAIL_ADDRESS)
-    else:
-        flash("Error. Please try again!", "danger")
-        return redirect("/birthday/billboard")
